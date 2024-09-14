44739681c38231b83c00b6ec3fb7496639a2c693
mmagleby <143196512+mmagleby@users.noreply.github.com>
2024-09-13 23:50:12 -0600

    Add files via upload

---
+++ downloads/_vti_cnf/pcxter.bat
@@ -0,0 +1,10 @@
+vti_encoding:SR|utf8-nl
+vti_timelastmodified:TR|10 Mar 2001 21:12:22 -0000
+vti_extenderversion:SR|4.0.2.2717
+vti_syncofs_ftp.xmission.com\:21/public_html/eaw:TW|14 Apr 2002 22:22:00 -0000
+vti_syncwith_ftp.xmission.com\:21/public_html/eaw:TR|10 Mar 2001 21:12:22 -0000
+vti_syncwith_localhost\\d\:\\files\\my webs/d\:/files/my webs/eaw_orig:TR|10 Mar 2001 21:12:22 -0000
+vti_syncofs_ftp.xmission.com\:21/~mmagleby/public_html/eaw:TW|08 Feb 2006 14:01:00 -0000
+vti_syncwith_ftp.xmission.com\:21/~mmagleby/public_html/eaw:TR|10 Mar 2001 21:12:22 -0000
+vti_syncwith_max188.x10hosting.com\:21/public_html/max188/eaw:TR|10 Mar 2001 21:12:22 -0000
+vti_syncofs_max188.x10hosting.com\:21/public_html/max188/eaw:TW|17 Apr 2009 02:56:00 -0000
